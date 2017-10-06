3d9496906dbccf6f92a2ba9f08de64c9977787fb
KazuhisaTaniguchi <jjzoow96@aol.com>
2017-10-07 05:22:15 +0900

    from 23

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+import SearchForm from './SearchForm';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '谷口',
+    };
+  }
+  handlePlaceSubmit(place) {
+    console.log(place);
+  }
+  render() {
+    return (
+      <div>
+        <h1>緯度経度検索</h1>
+        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+      </div>
+    );
+  }
+}
+
+export default App;
