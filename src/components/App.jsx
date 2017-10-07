dce1e7d27b3742bdcea45f7e90cda99409a5d62a
KazuhisaTaniguchi <jjzoow96@aol.com>
2017-10-07 16:15:13 +0900

    from 24

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import SearchForm from './SearchForm';
+import GeocodeResult from './GeocodeResult';
+
+const GECODE_ENDPOINT =
+  'https://maps.googleapis.com/maps/api/geocode/json';
 
 class App extends Component {
   constructor(props) {
@@ -11,12 +16,23 @@ class App extends Component {
   }
   handlePlaceSubmit(place) {
     console.log(place);
+    axios.get(GECODE_ENDPOINT, { params: { address: place } })
+      .then((result) => {
+        console.log(result);
+      });
   }
   render() {
     return (
       <div>
         <h1>緯度経度検索</h1>
-        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+        <SearchForm
+          onSubmit={place => this.handlePlaceSubmit(place)}
+        />
+        <GeocodeResult
+          address={this.state.address}
+          lat={this.state.lat}
+          lng={this.state.lng}
+        />
       </div>
     );
   }
